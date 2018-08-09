9aae63dc37dfce353e679306f1457cb9cfb66d64
刘涛 <tao_liu@sui.com>
2018-08-09 16:55:05 +0800

    feat: 重构file模块代码

--- script/util/file.js
+++ script/util/file.js
@@ -1,14 +1,15 @@
 /*
  * @Author: liu_tao
- * @Date: 2018-08-09 14:28:48
+ * @Date: 2018-08-09 16:54:37
  * @LastEditors: liu_tao
- * @LastEditTime: 2018-08-09 14:28:48
- * @Description: file util
+ * @LastEditTime: 2018-08-09 16:54:37
+ * @Description: file tools
  */
 
 const mkdirp = require('mkdirp')
 const path = require('path')
 const fs = require('fs')
+const util = require('util')
 
 function copyFile(srcPath, targetPath) {
   var readstream = fs.createReadStream(srcPath)
@@ -16,29 +17,24 @@ function copyFile(srcPath, targetPath) {
   readstream.pipe(writestream)
 }
 
-function copyFolder(templatePath, projectPath) {
-  fs.readdir(templatePath, function(err, files) {
-    if (err) {
-      console.warn(err)
-    } else {
-      files.forEach(function(filename) {
-        var srcDir = path.join(templatePath, filename)
-        var targetDir = path.join(projectPath, filename)
-        fs.stat(srcDir, function(error, stats) {
-          if (!error) {
-            var isFile = stats.isFile()
-            var isDir = stats.isDirectory()
-            if (isFile) {
-              copyFile(srcDir, targetDir)
-            }
-            if (isDir) {
-              createProjectFolder(targetDir)
-              copyFolder(srcDir, targetDir)
-            }
-          }
-        })
-      })
-    }
+async function stats(srcDir, targetDir) {
+  let stats = await util.promisify(fs.stat)(srcDir)
+  let isFile = stats.isFile()
+  let isDir = stats.isDirectory()
+  if (isFile) {
+    copyFile(srcDir, targetDir)
+  } else if (isDir) {
+    createProjectFolder(targetDir)
+    copyFolder(srcDir, targetDir)
+  }
+}
+
+async function copyFolder(templatePath, projectPath) {
+  let files = await util.promisify(fs.readdir)(templatePath)
+  files.forEach(filename => {
+    let srcDir = path.join(templatePath, filename)
+    let targetDir = path.join(projectPath, filename)
+    stats(srcDir, targetDir)
   })
 }
 
